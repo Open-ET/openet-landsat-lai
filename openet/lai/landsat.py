8f9dc55f08073768e1e68648e6d1e50c84ada165
Charles Morton <charles.g.morton@gmail.com>
2020-04-07 19:02:30 -0700

    Simplified functions and image construction for LAI and NLCD
    Added basic water masking which will set all water pixels to have an LAI of zero.
    
    Added LAI for each biome with where statement.  Instead of building an Image collection and reducing.  This seems to better "preserve" the input image and avoid needing reproject, clip, and updateMask calls.
    
    Simplified how NLCD image is selected by filtering the NLCD collection to the target system:index.
    
    Removed getLAI, trainModels, and trainRf functions.

--- openet/lai/landsat.py
+++ openet/lai/landsat.py
@@ -12,39 +12,36 @@ def getLAIImage(image, sensor, nonveg):
         sensor: needs to be specified as a String ('LT05', 'LE07', 'LC08')
         nonveg: True if want to compute LAI for non-vegetation pixels
     """
+    # Add necessary bands to image
     # image = renameLandsat(image)
-    rf_models = trainModels(sensor, nonveg)
-    laiImg = getLAI(image, rf_models)
+    train_img = getTrainImg(image)
 
-    # TODO: Test removing final clip call
-    #   It will currently cause an unbounded export error
-    return ee.Image(laiImg).clip(image.geometry())
-    # return ee.Image(laiImg)
-
-
-def trainModels(sensor, nonveg):
-    """
-    Function that trains biome-specific RF models for a specific sensor
-    Args:
-        sensor: str {'LT05', 'LE07', 'LC08'}
-        nonveg: bool, if True compute LAI for non-vegetation pixels
-    """
+    # Start with an image of all zeros
+    lai_img = train_img.select(['mask'], ['LAI']).multiply(0).double()
 
     if nonveg:
         biomes = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-        # biomes_str = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
     else:
         biomes = [1, 2, 3, 4, 5, 6, 7, 8]
-        # biomes_str = ['1', '2', '3', '4', '5', '6', '7', '8']
 
-    # Get models for each biome
-    # CM - Testing defining rf_modelsas a client side dictionary of classifiers
-    rf_models = {biome: getRFModel(sensor, biome) for biome in biomes}
+    # Apply LAI for each biome
+    for biome in biomes:
+        lai_img = lai_img.where(
+            train_img.select('biome2').eq(biome),
+            getLAIforBiome(train_img, biome, getRFModel(sensor, biome)))
 
-    # rf_models = ee.List(biomes).map(lambda biome: getRFModel(sensor, biome))
-    # rf_models = ee.Dictionary.fromLists(biomes_str, rf_models)
+    # Set water LAI to zero
+    # TODO: This should probably be in a separate function
+    # TODO: Check what water_mask the other models are using (PTJPL?)
+    water_mask = train_img.select('NDVI').lt(0) \
+        .And(train_img.select('nir').lt(0.1))
+    # water_mask = train_img.select('NDVI').lt(0) \
+    #     .And(train_img.select('NDWI').gt(0))
+    lai_img = lai_img.where(water_mask, 0)
 
-    return rf_models
+    # CM - copyProperties drops the type
+    return ee.Image(lai_img.copyProperties(image)) \
+        .set('system:time_start', image.get('system:time_start'))
 
 
 def getRFModel(sensor, biome):
@@ -66,58 +63,16 @@ def getRFModel(sensor, biome):
     if biome > 0:
         training_coll = training_coll.filterMetadata('biome2', 'equals', biome)
 
-    # TODO: Move trainRF code to this function
-    #   It doesn't seem that useful to have it separate
-    rf = trainRF(
-        samples=training_coll,
-        inputProperties=['red', 'green', 'nir', 'swir1', 'lat', 'lon',
-                         'NDVI', 'NDWI', 'sun_zenith', 'sun_azimuth'],
-        classProperty='MCD_LAI')
+    inputProperties = ['red', 'green', 'nir', 'swir1', 'lat', 'lon',
+                       'NDVI', 'NDWI', 'sun_zenith', 'sun_azimuth']
 
-    return rf
-
-
-def trainRF(samples, inputProperties, classProperty='MCD_LAI'):
-    """
-    Function that trains a Random Forest regressor
-    """
-    rfRegressor = ee.Classifier.randomForest(numberOfTrees=100,
-                                             minLeafPopulation=20,
-                                             variablesPerSplit=8) \
-                                .setOutputMode('REGRESSION') \
-                                .train(features=samples,
-                                       classProperty=classProperty,
-                                       inputProperties=inputProperties)
-
-    return rfRegressor
-
-
-def getLAI(image, rf_models):
-    """
-    Function that computes LAI for an input Landsat image and Random Forest models
-    """
-
-    # Add necessary bands to image
-    image = getTrainImg(image)
-
-    # TODO: Test building as images (not collection) to preserve input image
-
-    # Apply regressor for each biome
-    # CM - Testing defining rf_models as a client side dictionary of classifiers
-    lai_coll = ee.ImageCollection([
-        getLAIforBiome(image, biom, rf_model)
-        for biom, rf_model in rf_models.items()])
-    # biomes = rf_models.keys()
-    # lai_coll = ee.List(biomes).map(lambda b: getLAIforBiome(image, b, rf_models))
-    # lai_coll = ee.ImageCollection(lai_coll)
-
-    # Combine LAI of all biomes
-    # TODO: Test using a mosaic reducer
-    lai_img = ee.Image(lai_coll.mean().copyProperties(image)) \
-        .updateMask(image.select(['mask'])) \
-        .set('system:time_start', image.get('system:time_start'))
-
-    return lai_img
+    return ee.Classifier.randomForest(numberOfTrees=100,
+                                      minLeafPopulation=20,
+                                      variablesPerSplit=8) \
+                        .setOutputMode('REGRESSION') \
+                        .train(features=training_coll,
+                               classProperty='MCD_LAI',
+                               inputProperties=inputProperties)
 
 
 # TODO: Change name of "image" to something that indicates it has training bands
@@ -129,7 +84,8 @@ def getLAIforBiome(image, biome, rf_model):
         biome: int
         rf_model: ee.Classifier
     """
-    biom_lai = image.updateMask(image.select('biome2').eq(ee.Number(biome))) \
+    biom_lai = image\
+        .updateMask(image.select('biome2').eq(ee.Number(biome))) \
         .classify(rf_model, 'LAI')
     return biom_lai
 
@@ -139,31 +95,30 @@ def getTrainImg(image):
     Function that takes an Landsat image and prepare feature bands
     """
 
-    # TODO: Change so that the target year image is selected directly
-    #   instead of selecting from a merged image of all the years
     # Get NLCD for corresponding year
-    year = ee.Date(image.get('system:time_start')).get('year')
-    nlcd_all = ee.Image('USGS/NLCD/NLCD2001').select(['landcover'], ['landcover_2001']) \
-        .addBands(ee.Image('USGS/NLCD/NLCD2004').select(['landcover'], ['landcover_2004'])) \
-        .addBands(ee.Image('USGS/NLCD/NLCD2006').select(['landcover'], ['landcover_2006'])) \
-        .addBands(ee.Image('USGS/NLCD/NLCD2008').select(['landcover'], ['landcover_2008'])) \
-        .addBands(ee.Image('USGS/NLCD/NLCD2011').select(['landcover'], ['landcover_2011'])) \
-        .addBands(ee.Image('USGS/NLCD/NLCD2013').select(['landcover'], ['landcover_2013'])) \
-        .addBands(ee.Image('USGS/NLCD/NLCD2016').select(['landcover'], ['landcover_2016']))
+    # CM - I couldn't decide if it was better to use ints or strings
     nlcd_dict = {
-        '1997':0, '1998':0, '1999':0, '2000':0, '2001':0, '2002':0, 
-        '2003':1, '2004':1, '2005':1,
-        '2006':2, '2007':2, 
-        '2008':3, '2009':3, 
-        '2010':4, '2011':4, '2012':4,
-        '2013':5, '2014':5,
-        '2015':6, '2016':6, '2017':6, '2018':6, '2019':6, '2020':6,
+        '2001': ['1997', '1998', '1999', '2000', '2001', '2002'],
+        '2004': ['2003', '2004', '2005'],
+        '2006': ['2006', '2007'],
+        '2008': ['2008', '2009'],
+        '2011': ['2010', '2011', '2012'],
+        '2013': ['2013', '2014'],
+        '2016': ['2015', '2016', '2017', '2018', '2019', '2020'],
     }
-    nlcd_dict = ee.Dictionary(nlcd_dict)
-    nlcd_img = nlcd_all.select([nlcd_dict.get(ee.Number(year).format('%d'))])
+    nlcd_dict = ee.Dictionary({
+        src_year: tgt_year
+        for tgt_year, src_years in nlcd_dict.items()
+        for src_year in src_years})
+    nlcd_year = nlcd_dict.get(
+        ee.Date(image.get('system:time_start')).get('year').format('%d'))
+    nlcd_img = ee.ImageCollection('USGS/NLCD') \
+        .filter(ee.Filter.eq('system:index', ee.String('NLCD').cat(nlcd_year))) \
+        .first()
 
-    # CM - Not applying fmask (cloud/snow/water/etc)
-    # image = maskLST(image)
+    # CM - Add the NLCD year as a property to track which year was used
+    #   This probably isn't needed long term but it is useful for testing
+    image = image.set({'nlcd_year': nlcd_year})
 
     # Add the vegetation indices as additional bands
     image = getVIs(image)
@@ -197,12 +152,6 @@ def getTrainImg(image):
         .addBands(mask_img.float().add(ee.Number(image.get('SOLAR_AZIMUTH_ANGLE')))
                     .rename(['sun_azimuth'])) \
         .addBands(mask_img.add(1))
-        # CM - NLCD, path, and row are not currently, commenting out for now
-        # .addBands(mask_img.add(nlcd_img).rename(['nlcd'])) \
-        # .addBands(mask_img.add(ee.Number(image.get('WRS_PATH'))).rename(['path'])) \
-        # .addBands(mask_img.add(ee.Number(image.get('WRS_ROW'))).rename(['row'])) \
-        # .addBands(ee.Image.constant(ft.get('year')).rename(['year'])))
-        # .addBands(ee.Image.constant(ee.Number(ft.get('doy'))).rename(['DOY'])))
 
     # # CM - Test adding all bands directly and the calling updateMask to clip
     # mask_img = image.select(['pixel_qa'], ['mask']).multiply(0)
@@ -213,18 +162,8 @@ def getTrainImg(image):
     #               .rename(['sun_zenith'])) \
     #     .addBands(ee.Image.constant(ee.Number(image.get('SOLAR_AZIMUTH_ANGLE')))
     #               .rename(['sun_azimuth'])) \
-    #     .addBands(mask_img.add(1))
+    #     .addBands(mask_img.add(1)) \
     #     .updateMask(mask_img.add(1))
-    #     # CM - NLCD, path, and row are not currently, commenting out for now
-    #     # .addBands(nlcd_img.rename(['nlcd'])) \
-    #     # .addBands(ee.Image.constant(ee.Number(image.get('WRS_PATH'))).rename(['path'])) \
-    #     # .addBands(ee.Image.constant(ee.Number(image.get('WRS_ROW'))).rename(['row'])) \
-    #     # .addBands(ee.Image.constant(ft.get('year')).rename(['year'])))
-    #     # .addBands(ee.Image.constant(ee.Number(ft.get('doy'))).rename(['DOY'])))
-
-    # CM - Add the NLCD band name as a property to see which year was selected
-    #   This probably isn't needed long term but it is useful for testing
-    image = image.set({'nlcd_year': nlcd_img.select([0]).bandNames().get(0)})
 
     return image
 
@@ -233,48 +172,13 @@ def getVIs(image):
     """
     Compute VIs for an Landsat image
     """
-    NDVI = image.expression(
+    ndvi_img = image.expression(
         'float((b("nir") - b("red"))) / (b("nir") + b("red"))')
-    NDWI = image.expression(
+    ndwi_img = image.expression(
         'float((b("nir") - b("swir1"))) / (b("nir") + b("swir1"))')
 
-    # CM - SR is not currently used, commenting out for now
-    # SR = image.expression('float(b("nir")) / b("red")')
-
-    # CM - EVI is not currently used, commenting out for now
-    # CM - This equation is only correct for the raw scaled (0-10000) SR images
-    # EVI = image.expression(
-    #     '2.5 * float((b("nir") - b("red"))) / '
-    #     '(b("nir") + 6*b("red") - 7.5*float(b("blue")) + 10000)')
-    # CM - This equation should be used for the unscaled (0-1) SR images
-    # EVI = image.expression(
-    #     '2.5 * float((b("nir") - b("red"))) / '
-    #     '(b("nir") + 6*b("red") - 7.5 * float(b("blue")) + 1.0)')
-
-    # CM - These equations may need to be modified if not using the raw scaled SR
-    # GCI = image.expression('float(b("nir")) / b("green") - 1')
-    # EVI2 = image.expression(
-    #     '2.5 * float((b("nir") - b("red"))) / '
-    #     '(b("nir") + 2.4 * float(b("red")) + 10000)')
-    # OSAVI = image.expression(
-    #     '1.16 * float(b("nir") - b("red")) / (b("nir") + b("red") + 1600)')
-    # NDWI2 = image.expression(
-    #     'float((b("nir") - b("swir2"))) / (b("nir") + b("swir2"))')
-    # MSR = image.expression('float(b("nir")) / b("swir1")')
-    # MTVI2 = image.expression(
-    #     '1.5 * (1.2 * float(b("nir") - b("green")) - 2.5 * float(b("red") - b("green"))) / '
-    #     'sqrt((2*b("nir")+10000)*(2*b("nir")+10000) - (6*b("nir") - 5*sqrt(float(b("nir"))))-5000)')
-
-    return image.addBands(NDVI.select([0], ['NDVI'])) \
-                .addBands(NDWI.select([0], ['NDWI']))
-                # .addBands(EVI.select([0], ['EVI']))
-                # .addBands(SR.select([0], ['SR'])) \
-                # .addBands(GCI.select([0], ['GCI']))
-                # .addBands(EVI2.select([0], ['EVI2']))
-                # .addBands(OSAVI.select([0], ['OSAVI']))
-                # .addBands(NDWI2.select([0], ['NDWI2']))
-                # .addBands(MSR.select([0], ['MSR']))
-                # .addBands(MTVI2.select([0], ['MTVI2']))
+    return image.addBands(ndvi_img.select([0], ['NDVI'])) \
+                .addBands(ndwi_img.select([0], ['NDWI']))
 
 
 def renameLandsat(image):
@@ -316,12 +220,3 @@ def renameLandsat(image):
 #     # Return a single band image of the extracted QA bits, giving the band
 #     # a new name.
 #     return image.select([0], [newName]).bitwiseAnd(pattern).rightShift(start)
-
-
-# DEADBEEF - Function is not used anymore
-# def setDate(image):
-#     """
-#     Function that adds a "date" property to an image in format "YYYYmmdd"
-#     """
-#     image_date = ee.Date(image.get('system:time_start'))
-#     return image.set('date', image_date.format('YYYYMMdd'))
--- openet/lai/tests/test_landsat.py
+++ openet/lai/tests/test_landsat.py
@@ -42,20 +42,18 @@ def test_getVIs_bands():
     input_bands = input_img.bandNames().getInfo()
     output = openet.lai.landsat.getVIs(input_img).bandNames().getInfo()
     assert set(output) == set(input_bands) | {'NDVI', 'NDWI'}
-    # CM - SR and EVI are not used in the classification, commenting out for now
-    # assert set(output) == set(input_bands) | {'SR', 'NDVI', 'EVI', 'NDWI'}
 
 
 @pytest.mark.parametrize(
-    "blue, red, nir, swir1, sr, ndvi, evi, ndwi",
+    "blue, red, nir, swir1, ndvi, ndwi, evi, sr",
     [
         # Raw scaled (0-10000) SR values
-        [1000, 2000, 8000, 3000, 4.0, 0.6, 0.6666, 0.4545],
+        [1000, 2000, 8000, 3000, 0.6, 0.4545, 4.0, 0.6666],
         # Unscaled (0-1) SR values
-        # [0.1, 0.2, 0.8, 0.3, 0.6, 0.6, 0.6, 0.6],
+        # [0.1, 0.2, 0.8, 0.3, 0.6, 0.4545, 4.0, 0.6666],
     ]
 )
-def test_getVIs_constant_values(blue, red, nir, swir1, sr, ndvi, evi, ndwi,
+def test_getVIs_constant_values(blue, red, nir, swir1, ndvi, ndwi, evi, sr,
                                 tol=0.01):
     # Check that the VI calculations are valid using constant images
     input_img = ee.Image.constant([blue, red, nir, swir1])\
@@ -64,28 +62,19 @@ def test_getVIs_constant_values(blue, red, nir, swir1, sr, ndvi, evi, ndwi,
     assert abs(output['NDVI'] - ndvi) <= tol
     assert abs(output['NDWI'] - ndwi) <= tol
 
-    # CM - SR and EVI are not used in the classification, commenting out for now
-    # assert abs(output['EVI'] - evi) <= tol
-    # assert abs(output['SR'] - sr) <= tol
-
 
 @pytest.mark.parametrize(
-    "image_id, xy, sr, ndvi, ndwi, evi",
+    "image_id, xy, ndvi, ndwi, evi, sr",
     [
-        [LANDSAT8_IMAGE_ID, TEST_POINT, 14.9227, 0.8744, 0.5043, 0.5301],
+        [LANDSAT8_IMAGE_ID, TEST_POINT, 0.8744, 0.5043, 0.5301, 14.9227],
     ]
 )
-def test_getVIs_point_values(image_id, xy, sr, ndvi, evi, ndwi, tol=0.0001):
+def test_getVIs_point_values(image_id, xy, ndvi, ndwi, evi, sr, tol=0.0001):
     # Check that the VI calculations are valid at specific points
     input_img = openet.lai.landsat.renameLandsat(ee.Image(LANDSAT8_IMAGE_ID))
-    pprint.pprint(utils.point_image_value(input_img, xy=xy))
     output = utils.point_image_value(openet.lai.landsat.getVIs(input_img), xy=xy)
     assert abs(output['NDVI'] - ndvi) <= tol
     assert abs(output['NDWI'] - ndwi) <= tol
-
-    # CM - SR and EVI are not used in the classification, commenting out for now
-    # assert abs(output['EVI'] - evi) <= tol
-    # assert abs(output['SR'] - sr) <= tol
 
 
 def test_getTrainImg_bands():
@@ -93,10 +82,6 @@ def test_getTrainImg_bands():
     # Both the VI and training bands get added in getTrainImg
     vi_bands = {'NDVI', 'NDWI'}
     training_bands = {'biome2', 'lon', 'lat', 'sun_zenith', 'sun_azimuth', 'mask'}
-    # CM - SR and EVI are not used in the classification, commenting out for now
-    # vi_bands = {'SR', 'NDVI', 'NDWI', 'EVI'}
-    # training_bands = {'nlcd', 'lon', 'lat', 'path', 'row', 'sun_zenith',
-    #                   'sun_azimuth', 'biome2'}
     target_bands = input_bands | vi_bands | training_bands
     input_img = openet.lai.landsat.renameLandsat(ee.Image(LANDSAT8_IMAGE_ID))
     output_bands = openet.lai.landsat.getTrainImg(input_img).bandNames().getInfo()
@@ -107,21 +92,21 @@ def test_getTrainImg_bands():
     "date, nlcd_band",
     [
         # CM - We don't really need to test all of these
-        ['2003-01-01', 'landcover_2004'],
-        ['2007-01-01', 'landcover_2006'],
-        ['2008-01-01', 'landcover_2008'],
-        ['2012-01-01', 'landcover_2011'],
+        ['2003-01-01', '2004'],
+        ['2007-01-01', '2006'],
+        ['2008-01-01', '2008'],
+        ['2012-01-01', '2011'],
         # Check if the transition at the new year is handled
-        ['2014-12-31', 'landcover_2013'],
-        ['2015-01-01', 'landcover_2016'],
+        ['2014-12-31', '2013'],
+        ['2015-01-01', '2016'],
         # Check the supported start/end years
-        ['1997-01-01', 'landcover_2001'],
-        ['2020-01-01', 'landcover_2016'],
+        ['1997-01-01', '2001'],
+        ['2020-01-01', '2016'],
         # # What should happen for years outside the supported range
         # # Currently this will raise a EEException
         # # (about the dictionary not having the correct key)
-        # ['1996-01-01', 'landcover_1997'],
-        # ['2021-01-01', 'landcover_2016'],
+        # ['1996-01-01', '1997'],
+        # ['2021-01-01', '2016'],
     ]
 )
 def test_getTrainImg_nlcd_year(date, nlcd_band):
@@ -137,17 +122,11 @@ def test_getTrainImg_property_values():
         openet.lai.landsat.getTrainImg(input_img), xy=TEST_POINT)
     assert abs(output['lon'] - TEST_POINT[0]) <= 0.0001
     assert abs(output['lat'] - TEST_POINT[1]) <= 0.0001
-
     # CM - Hardcoding solar angles for now to avoid extra getInfo calls
     assert output['sun_azimuth'] == 127.089134
     assert output['sun_zenith'] == 25.720642
     # assert output['sun_azimuth'] == input_img.get('SOLAR_AZIMUTH_ANGLE').getInfo()
     # assert output['sun_zenith'] == input_img.get('SOLAR_ZENITH_ANGLE').getInfo()
-
-    # CM - path and row are not used in the classification, commenting out for now
-    # scene_id = LANDSAT8_IMAGE_ID.split('/')[-1]
-    # assert output['path'] == int(scene_id.split('_')[1][0:3])
-    # assert output['row'] == int(scene_id.split('_')[1][3:6])
 
 
 @pytest.mark.parametrize(
@@ -183,29 +162,6 @@ def test_getTrainImg_biome_point_values(image_id, xy, nlcd, biome2):
 #     assert output['biome2'] is None
 
 
-def test_trainRF():
-    # CM - Get examples of "samples":
-    # sensor = 'LC08'
-    # assetDir = 'users/yanghui/OpenET/LAI_US/train_samples/'
-    # filename = 'LAI_train_samples_' + sensor + '_v10_1_labeled'
-    # train = ee.FeatureCollection(assetDir + filename)
-    # pprint.pprint(train.first().getInfo())
-    # # train = train.filterMetadata('sat_flag', 'equals', 'correct')
-    # # train = ee.Algorithms.If(ee.Number(biome).eq(0), train,
-    # #                          train.filterMetadata('biome2', 'equals', biome))
-    # # samples = ee.FeatureCollection(train)
-
-    # CM - What needs to be in samples to test if trainRF is working?
-    samples = ee.FeatureCollection([
-        ee.Feature(None, {'MCD_LAI': 1.0}),
-    ])
-    features = ['red', 'green', 'nir', 'swir1', 'lat', 'lon', 'NDVI', 'NDWI',
-                'sun_zenith', 'sun_azimuth']
-    output = openet.lai.landsat.trainRF(
-        samples, features, classProperty='MCD_LAI').getInfo()
-    assert output
-
-
 # CM - How do we test if the classifier is correct?
 #   Currently it is only testing if something is returned
 # def test_getRFModel(sensor='LC08', biome=0):
@@ -219,6 +175,12 @@ def test_trainRF():
         ['LC08', 0],
         ['LE07', 0],
         ['LT05', 0],
+        # ['LC08', 1],
+        # ['LE07', 1],
+        # ['LT05', 1],
+        # ['LC08', 2],
+        # ['LE07', 2],
+        # ['LT05', 2],
     ]
 )
 def test_getRFModel_sensor(sensor, biome):
@@ -240,26 +202,6 @@ def test_getRFModel_sensor_exception(sensor='LT04', biome=0):
         openet.lai.landsat.getRFModel(sensor, biome).getInfo()
 
 
-# TODO: Test that here is a separate classfier for each biome
-#   A test on the sensor probably isn't needed since that is passed through
-def test_trainModels():
-    output = openet.lai.landsat.trainModels(sensor='LE07', nonveg=True)
-    # pprint.pprint(output)
-    assert type(output) is dict
-    assert set(output.keys()) == set(range(0, 9))
-    assert output[1].getInfo()
-
-
-def test_trainModels_nonveg_true():
-    output = openet.lai.landsat.trainModels(sensor='LC08', nonveg=True)
-    assert 0 in output.keys()
-
-
-def test_trainModels_nonveg_false():
-    output = openet.lai.landsat.trainModels(sensor='LC08', nonveg=False)
-    assert 0 not in output.keys()
-
-
 @pytest.mark.parametrize(
     "image_id, xy, biome, expected",
     [
@@ -279,34 +221,10 @@ def test_getLAIforBiome_point_values(image_id, xy, biome, expected, tol=0.0001):
     input_img = openet.lai.landsat.getTrainImg(
         openet.lai.landsat.renameLandsat(ee.Image(image_id)))
     sensor = LANDSAT8_IMAGE_ID.split('/')[-1][:4]
-    rf_model = openet.lai.landsat.trainModels(sensor, nonveg=True)[biome]
+    rf_model = openet.lai.landsat.getRFModel(sensor, biome)
     output = utils.point_image_value(
         openet.lai.landsat.getLAIforBiome(input_img, biome, rf_model), xy=xy)
     assert abs(output['LAI'] - expected) <= tol
-
-
-# def test_getLAI():
-#     input_img = openet.lai.landsat.renameLandsat(ee.Image(LANDSAT8_IMAGE_ID))
-#     sensor = LANDSAT8_IMAGE_ID.split('/')[-1][:4]
-#     rf_models = openet.lai.landsat.trainModels(sensor, nonveg=True)
-#     output = openet.lai.landsat.getLAI(input_img, rf_models).getInfo()
-#     pprint.pprint(output)
-#     assert output
-#
-#     # # Add necessary bands to image
-#     # image = getTrainImg(image)
-#     # mask_prev = image.select([0]).mask()
-#     #
-#     # # Apply regressor for each biome
-#     # biomes = rf_models.keys()
-#     # lai_coll = ee.List(biomes).map(lambda b: getLAIforBiome(image, b, rf_models))
-#     # lai_coll = ee.ImageCollection(lai_coll)
-#     #
-#     # # combine lai of all biomes
-#     # lai_img = ee.Image(lai_coll.mean().copyProperties(image)).updateMask(mask_prev) \
-#     #     .set('system:time_start',image.get('system:time_start'))
-#     #
-#     # return lai_img
 
 
 def test_getLAIImage_band_name():
@@ -323,16 +241,6 @@ def test_getLAIImage_point_values(image_id=LANDSAT8_IMAGE_ID, xy=TEST_POINT,
     output = utils.point_image_value(
         openet.lai.landsat.getLAIImage(input_img, sensor, nonveg=1), xy=xy)
     assert abs(output['LAI'] - expected) <= tol
-
-
-# DEADBEEF - setDate function is commented out in landsat.py
-# def test_setDate():
-#     # Set 'date' property with EE format 'YYYYMMdd'
-#     input_date = '2017-07-16'
-#     input_img = ee.Image.constant(2) \
-#         .set({'system:time_start': ee.Date(input_date).millis()})
-#     output = openet.lai.landsat.setDate(input_img).get('date').getInfo()
-#     assert output == input_date.replace('-', '')
 
 
 # DEADBEEF - getQABits function is commented out in landsat.py
@@ -403,7 +311,6 @@ def test_getLAIImage_point_values(image_id=LANDSAT8_IMAGE_ID, xy=TEST_POINT,
 #     # Check that all non-clear pixels are masked
 #     input_img = openet.lai.landsat.renameLandsat(ee.Image(image_id))
 #     output = utils.point_image_value(openet.lai.landsat.maskLST(input_img), xy=xy)
-#     pprint.pprint(utils.point_image_value(input_img, xy=xy))
 #     assert output['nir'] is None
 #
 #

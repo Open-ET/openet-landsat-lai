4cca62463c66cb6e3979e4b077af155b28dbce0c
Charles Morton <charles.g.morton@gmail.com>
2020-04-09 15:34:04 -0700

    Update ee_Landsat_LAI_export_v4.py
    Made the wrs2 an input parameter and fixed some issues with setting the properties.

--- scripts/ee_Landsat_LAI_export_v4.py
+++ scripts/ee_Landsat_LAI_export_v4.py
@@ -18,7 +18,8 @@ TOOL_NAME = 'ee_Landsat_LAI_export'
 TOOL_VERSION = 'v4'
 
 
-def main(start_dt, end_dt, overwrite_flag=False, gee_key_file=None):
+def main(start_dt, end_dt, wrs2_tiles, overwrite_flag=False,
+         gee_key_file=None):
     """Export Landsat LAI images
 
     Parameters
@@ -29,6 +30,8 @@ def main(start_dt, end_dt, overwrite_flag=False, gee_key_file=None):
     end_dt : datetime, optional
         Override the (inclusive) end date in the INI file
         (the default is None which will use the INI end date).
+    wrs2_tiles : str, None, optional
+        List of WRS2 tiles to process.
     overwrite_flag : bool, optional
         If True, overwrite existing files (the default is False).
     gee_key_file : str, None, optional
@@ -46,21 +49,8 @@ def main(start_dt, end_dt, overwrite_flag=False, gee_key_file=None):
     end_date = end_dt.strftime('%Y-%m-%d')
 
     # CM - Hard coding input parameters for now
-    # wrs2_tile_list = ['p044r034']  # openet-api-gee
-    # wrs2_tile_list = ['p043r035']  # openet-dri-gee
-    wrs2_tile_list = ['p042r036']   # openet-gee
-    # wrs2_tile_list = [
-    #     'p044r032', 'p043r033', 'p042r034',
-    #     'p044r033', 'p043r034', 'p042r035', 'p044r034', 'p043r035', 'p042r036',
-    #     'p041r035', 'p041r036',
-    #     'p045r032', 'p045r033',
-    # ]
-    version = openet.lai.__version__.replace('.', 'p')
-    coll_id = 'projects/openet/lai/landsat/scene'
-    # gee_key_file = None
-    # gee_key_file = '/Users/mortonc/Projects/keys/openet-api-gee.json'
-    # gee_key_file = '/Users/mortonc/Projects/keys/openet-dri-gee.json'
-    gee_key_file = '/Users/mortonc/Projects/keys/openet-gee.json'
+    coll_id = 'projects/earthengine-legacy/assets/projects/openet/lai/landsat/scene'
+    # coll_id = 'projects/openet/lai/landsat/scene'
     cloud_cover_max = 70
     log_tasks = True
     clip_ocean_flag = True
@@ -68,6 +58,19 @@ def main(start_dt, end_dt, overwrite_flag=False, gee_key_file=None):
     output_type = 'uint16'
     model_name = 'LAI'
 
+    # Central Valley WRS2 tiles
+    # wrs2_tiles = [
+    #     'p044r033', 'p044r032', 'p044r034',  # openet
+    #     'p043r034', 'p043r033', 'p043r035',  # openet-dri
+    #     'p042r035', 'p042r034', 'p042r036',  # openet-api
+    #     'p041r035', 'p041r036',
+    #     'p045r032', 'p045r033',
+    # ]
+
+    logging.debug('User tiles: {}'.format(wrs2_tiles))
+    wrs2_tiles = [y.strip() for x in wrs2_tiles for y in x.split(',')]
+    wrs2_tiles = [x.lower() for x in wrs2_tiles if x]
+    logging.info('WRS2 tiles: {}'.format(', '.join(wrs2_tiles)))
 
     logging.info('\nInitializing Earth Engine')
     if gee_key_file:
@@ -102,7 +105,7 @@ def main(start_dt, end_dt, overwrite_flag=False, gee_key_file=None):
         ee.data.createAsset({'type': 'IMAGE_COLLECTION'}, coll_id)
 
 
-    for wrs2_tile in wrs2_tile_list:
+    for wrs2_tile in wrs2_tiles:
         path, row = int(wrs2_tile[1:4]), int(wrs2_tile[5:8])
 
         # Get a list of the available Landsat asset IDs
@@ -120,6 +123,9 @@ def main(start_dt, end_dt, overwrite_flag=False, gee_key_file=None):
             logging.debug('  {}'.format(input_asset_id))
             logging.debug('  {}'.format(output_asset_id))
 
+            export_id = '{}_LAI_{}'.format(
+                landsat_id, openet.lai.__version__.replace('.', 'p'))
+
             if ee.data.getInfo(output_asset_id):
                 if overwrite_flag:
                     logging.info('  asset already exists - overwriting')
@@ -130,19 +136,19 @@ def main(start_dt, end_dt, overwrite_flag=False, gee_key_file=None):
 
             # TODO: Module should handle the band renaming and scaling
             # Copied from PTJPL Image.from_landsat_c1_sr()
-            landsat_img = ee.Image(input_asset_id)
+            input_img = ee.Image(input_asset_id)
             input_bands = ee.Dictionary({
                 'LANDSAT_5': ['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'B6', 'pixel_qa'],
                 'LANDSAT_7': ['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'B6', 'pixel_qa'],
                 'LANDSAT_8': ['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B10', 'pixel_qa']})
             output_bands = ['blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'tir',
                             'pixel_qa']
-            spacecraft_id = ee.String(landsat_img.get('SATELLITE'))
-            prep_img = landsat_img \
+            spacecraft_id = ee.String(input_img.get('SATELLITE'))
+            prep_img = input_img \
                 .select(input_bands.get(spacecraft_id), output_bands) \
-                .set({'system:index': landsat_img.get('system:index'),
-                      'system:time_start': landsat_img.get('system:time_start'),
-                      'system:id': landsat_img.get('system:id'),
+                .set({'system:index': input_img.get('system:index'),
+                      'system:time_start': input_img.get('system:time_start'),
+                      'system:id': input_img.get('system:id'),
                       'SATELLITE': spacecraft_id,
                      })
             # CM - Don't unscale the images yet
@@ -155,31 +161,13 @@ def main(start_dt, end_dt, overwrite_flag=False, gee_key_file=None):
 
             # Get the projection as a server side object for production to avoid
             #   an extra getInfo call
-            image_proj = landsat_img.select(['B4']).projection()
+            image_proj = input_img.select(['B4']).projection()
             image_crs = image_proj.crs()
             image_transform = ee.List(ee.Dictionary(
                 ee.Algorithms.Describe(image_proj)).get('transform'))
 
             # Compute LAI image
             output_img = openet.lai.landsat.getLAIImage(prep_img, sensor, nonveg=1)
-
-            # Copied properties from scene export tool
-            # Many of these are so the tasks can be tracked in the datastore
-            properties = {
-                'coll_id': coll_id,
-                'date_ingested': datetime.datetime.today().strftime('%Y-%m-%d'),
-                'image_id': input_asset_id,
-                'lai_version': openet.lai.__version__,
-                'model_name': 'LAI',
-                'model_version': openet.lai.__version__,
-                'scale_factor': 1.0 / scale_factor,
-                'scene_id': landsat_id,
-                'tool_name': TOOL_NAME,
-                'tool_version': TOOL_VERSION,
-                'wrs2_tile': 'p{}r{}'.format(
-                    landsat_id.split('_')[1][:3], landsat_id.split('_')[1][3:]),
-            }
-            output_img = output_img.set(properties)
 
             if scale_factor > 1:
                 if output_type == 'int16':
@@ -188,15 +176,43 @@ def main(start_dt, end_dt, overwrite_flag=False, gee_key_file=None):
                 elif output_type == 'uint16':
                     output_img = output_img.multiply(scale_factor).round()\
                         .clamp(0, 65536).uint16()
+                else:
+                    raise ValueError('unsupported combination of output_type '
+                                     'and scale_factor')
 
             if clip_ocean_flag:
                 output_img = output_img \
                     .updateMask(ee.Image('projects/openet/ocean_mask'))
 
+            # Copied properties from scene export tool
+            # Many of these are so the tasks can be tracked in the datastore
+            input_info = input_img.getInfo()
+            properties = {
+                'coll_id': coll_id,
+                'date_ingested': datetime.datetime.today().strftime('%Y-%m-%d'),
+                'image_id': input_asset_id,
+                'lai_version': openet.lai.__version__,
+                'model_name': model_name,
+                'model_version': openet.lai.__version__,
+                'scale_factor': 1.0 / scale_factor,
+                'scene_id': landsat_id,
+                'tool_name': TOOL_NAME,
+                'tool_version': TOOL_VERSION,
+                'wrs2_tile': 'p{}r{}'.format(
+                    landsat_id.split('_')[1][:3], landsat_id.split('_')[1][3:]),
+                'CLOUD_COVER': input_info['properties']['CLOUD_COVER'],
+                'CLOUD_COVER_LAND': input_info['properties']['CLOUD_COVER_LAND'],
+                'system:time_start': input_info['properties']['system:time_start'],
+            }
+            output_img = output_img.set(properties)
+
+            # CM - Copy all the properties from the source image
+            # output_img = ee.Image(output_img.copyProperties(input_img))
+
             # Start export
             task = ee.batch.Export.image.toAsset(
                 image=output_img,
-                description='{}_LAI_{}'.format(landsat_id, version),
+                description=export_id,
                 assetId=output_asset_id,
                 crs=image_crs,
                 crsTransform=image_transform,
@@ -296,6 +312,9 @@ def arg_parse():
         '--key', type=utils.arg_valid_file, metavar='FILE',
         help='Earth Engine service account JSON key file')
     parser.add_argument(
+        '--tiles', default='', nargs='+',
+        help='Comma/space separated list of tiles to process')
+    parser.add_argument(
         '--overwrite', default=False, action='store_true',
         help='Force overwrite of existing files')
     parser.add_argument(
@@ -314,6 +333,6 @@ if __name__ == "__main__":
     logging.basicConfig(level=args.loglevel, format='%(message)s')
     logging.getLogger('googleapiclient').setLevel(logging.ERROR)
 
-    main(start_dt=args.start, end_dt=args.end,
+    main(start_dt=args.start, end_dt=args.end, wrs2_tiles=args.tiles,
          overwrite_flag=args.overwrite, gee_key_file=args.key,
          )
